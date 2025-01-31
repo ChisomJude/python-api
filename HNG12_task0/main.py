0e8eb12d1fa08e851600d305dfef863c57981f48
chisomjude <judegodstimechisom@gmail.com>
2025-01-31 09:08:15 +0100

    modified time and current time style

--- HNG12_task0/main.py
+++ HNG12_task0/main.py
@@ -1,22 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
+import pytz
+
 
 app = FastAPI()
 
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (change this for security)
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["GET"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"], 
 )
 
+timezone = pytz.timezone("Africa/Lagos")
+
 @app.get("/")
-def read_root():
-    return {
-        "email": "chisomjude0205@gmail.com",
-        "current_datetime":datetime.now(timezone.utc).isoformat(timespec='seconds') + "Z",
-        "github_url": "https://github.com/ChisomJude/python-api/tree/master/HNG12_task0"
+
+def mygetapi():
+    email = "chisomjude0205@gmail.com"
+    current_datetime=(datetime.now()).isoformat()+ "Z"
+    github_url ="https://github.com/ChisomJude/hngtask0"
+    response = {
+        "email": email,
+        "current_datetime" : current_datetime,
+        "github_url": github_url,
     }
+    return response
+
